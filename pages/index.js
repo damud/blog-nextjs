e6d67cd9b06fa6d3f5ecc29e41e61c332e11e0b6
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-03-27 19:58:16 +0200

    Styled search input in header component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Header from '../components/header';
 export default function Home() {
   return (
- <h1 className='text-3xl font-bold underline'>
-  Hello
- </h1>
-  )
+    <Header>
+
+    </Header>
+    )
 }
+ 
