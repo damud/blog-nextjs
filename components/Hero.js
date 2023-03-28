767f3959a4f7b1da545c0cd1e8a97b75380acb6b
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-03-28 12:26:41 +0200

    Created category links

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Image from "next/image"
 
 const Hero = () => {
@@ -5,9 +6,10 @@ const Hero = () => {
     <section className="py-16">
       <div className="container mx-auto md:px-20">
         <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
+        {Slide()}
       </div>
     </section>
-
+    
   )
 }
 
@@ -17,9 +19,13 @@ const Slide = () => {
   return (
     <div className="grid md:grid-cols-2">
       <div className="image">
-        <Image src="" width={600} height={600} />
+        <Link href=""><Image src="/images/img1.jpg" width={600} height={600} alt="" /></Link>
       </div>
       <div className="info"></div>
+      <div className="category">
+        <Link href="/" className="text-orange-600 hover:text-orange-400">Business, Travel</Link>
+        <Link href="/" className="text-gray-800 hover:text-gray-600">- 28/03/2023</Link>
+      </div>
     </div>
   )
 }
