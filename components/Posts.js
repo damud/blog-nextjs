ebd067c6bfe0599f1293f387896459d83d232445
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-03-30 11:19:15 +0200

    Created most popular posts section

---
+++ components/Posts.js
@@ -0,0 +1,49 @@
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import Link from "next/link"
+import Image from "next/image" 
+import Author from "./_child/Author"
+
+const Posts = () => {
+  return (
+    <section className="container mx-auto md:px-20 py-16">
+        <h1 className="font-bold text-4xl py-12 text-center">Most Popular</h1>
+    <Swiper
+       slidesPerView={2}
+    >
+        <SwiperSlide>{Post()}</SwiperSlide>
+        <SwiperSlide>{Post()}</SwiperSlide>
+        <SwiperSlide>{Post()}</SwiperSlide>
+        <SwiperSlide>{Post()}</SwiperSlide>
+        <SwiperSlide>{Post()}</SwiperSlide>
+        <SwiperSlide>{Post()}</SwiperSlide>
+    </Swiper>
+    </section>
+  )
+}
+
+const Post = () => {
+    return (
+      <div className="item">
+        <div className="images">
+        <Link href="/"><Image src="/images/img1.jpg" width={600} height={400} alt="" /></Link>
+        </div>
+        <div className="info flex justify-center flex-col py-4">
+          <div className="category">
+              <Link href="/" className="text-orange-600 hover:text-orange-800">Business, Travel</Link>
+              <Link href="/" className="text-gray-800 hover:text-gray-600">- 28/03/2023</Link>
+          </div>
+          <div className="title">
+              <Link href="/" className="text-3xl md:text-4xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</Link>
+          </div>
+          <p className="text-gray-500 py-3">
+              Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
+              text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+          </p>
+          <Author></Author>
+        </div>
+      </div>
+    )
+  }
+
+export default Posts
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import Layout from '@/layout/Layout';
 // Sections
 import Hero from '@/components/Hero';
 import Blog from '@/components/Blog';
+import Posts from '@/components/Posts';
 
 export default function Home() {
   return (
     <Layout>
      <Hero></Hero>
      <Blog></Blog>
+     <Posts></Posts>
     </Layout>
     )
 }
