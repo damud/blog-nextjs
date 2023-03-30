00caca9505ed1b169b62547e2fbaa4e590950ed5
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-03-30 12:34:10 +0200

    Add newsletter to footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+import Newsletter from "./_child/newsletter";
 
 const Footer = () => {
   return (
     <div className="bg-gray-50">
       <div className="container mx-auto flex justify-center py-12">
         <div className="py-5">
+          <Newsletter></Newsletter>
           <div className="flex justify-center gap-6">
           <Link href="/"><ImFacebook color="#888"/></Link>
           <Link href="/"><ImTwitter color="#888"/></Link>
