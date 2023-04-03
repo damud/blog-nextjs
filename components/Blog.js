434b1dea26dc47bf8e451dc7545965b704fc2be1
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-04-03 19:21:56 +0200

    Fetched blog posts data from file data.js

--- components/Blog.js
+++ components/Blog.js
@@ -5,42 +5,40 @@ import fetcher from "@/library/fetcher";
 
 const blog = () => {
     const {data, isLoading, isError} = fetcher("api/posts");
-    if(data) console.log(data);
+   
+    if(isLoading) return <div>Loading...</div>
+    if(isError) return <div>Error</div>
   return (
     <section className="container mx-auto md:px-20 py-10">
       <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
+       {
+        data.map((value, index)=>(
+          <Post data={value} key={index}></Post>
+        ))
+       }
       </div>
     </section>
   )
 }
 
-const Post = () => {
+const Post = ({data}) => {
+  const {id, title, subtitle, category, img, published, author} = data;
   return (
     <div className="item">
       <div className="images">
-      <Link href="/"><Image src="/images/img1.jpg" className="rounded" width={500} height={35} alt="" /></Link>
+      <Link href="/"><Image src={img || "/"} className="rounded" width={500} height={35} alt="" /></Link>
       </div>
       <div className="info flex justify-center flex-col py-4">
         <div className="category">
-            <Link href="/" className="text-orange-600 hover:text-orange-800">Business, Travel</Link>
-            <Link href="/" className="text-gray-800 hover:text-gray-600">- 28/03/2023</Link>
+            <Link href="/" className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</Link>
+            <Link href="/" className="text-gray-800 hover:text-gray-600">- {published || "Unknown"}</Link>
         </div>
         <div className="title">
-            <Link href="/" className="text-xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</Link>
+            <Link href="/" className="text-xl font-bold text-gray-800 hover:text-gray-600">{title || "Title"}</Link>
         </div>
-        <p className="text-gray-500 py-3">
-            Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-            text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
-        </p>
-        <Author></Author>
+        <p className="text-gray-500 py-3">{subtitle || "Subtitle"}</p>
+        {author?<Author></Author>:<></>}
       </div>
     </div>
   )
