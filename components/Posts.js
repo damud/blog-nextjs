f0fd0ad268d48167f0ba6e5a8ac932b22557f91e
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-04-04 11:25:46 +0200

    Fetch data in Blog section

--- components/Posts.js
+++ components/Posts.js
@@ -1,41 +1,54 @@
 
-import { Swiper, SwiperSlide } from "swiper/react"
-import Link from "next/link"
-import Image from "next/image" 
-import Author from "./_child/Author"
+import { Swiper, SwiperSlide } from "swiper/react";
+import Link from "next/link";
+import Image from "next/image"; 
+import Author from "./_child/Author";
+import fetcher from "@/library/fetcher";
+import Loader from "./_child/Loader";
+import Error from "./_child/Error";
 
 const Posts = () => {
+  
+  const { data, isLoading, isError } = fetcher('api/popular');
+  
+  if(isLoading) return <Loader />
+  if(isError) return <Error />
+
   return (
     <section className="container mx-auto md:px-20 py-16">
         <h1 className="font-bold text-4xl py-12 text-center">Most Popular</h1>
     <Swiper
        slidesPerView={2}
     >
-        <SwiperSlide>{Post()}</SwiperSlide>
+        {
+          data.map((value, index)=> (
+            <SwiperSlide key={index}><Post data={value}></Post></SwiperSlide>
+          ))
+        }
     </Swiper>
     </section>
   )
 }
 
-const Post = () => {
+const Post = ({data}) => {
+  const {id, title, category, img, published, description, author} = data;
     return (
       <div className="item">
         <div className="images">
-        <Link href="/"><Image src="/images/img1.jpg" width={400} height={400} alt="" /></Link>
+        <Link href="/"><Image src={img} width={400} height={400} alt="" /></Link>
         </div>
         <div className="info flex justify-center flex-col py-4">
           <div className="category">
-              <Link href="/" className="text-orange-600 hover:text-orange-800">Business, Travel</Link>
-              <Link href="/" className="text-gray-800 hover:text-gray-600">- 28/03/2023</Link>
+              <Link href="/" className="text-orange-600 hover:text-orange-800">{category || "No Category"}</Link>
+              <Link href="/" className="text-gray-800 hover:text-gray-600">- {published || "28/03/2023"}</Link>
           </div>
           <div className="title">
-              <Link href="/" className="text-3xl md:tezt-4xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</Link>
+              <Link href="/" className="text-3xl md:tezt-4xl font-bold text-gray-800 hover:text-gray-600">{title || "No Title"}</Link>
           </div>
           <p className="text-gray-500 py-3">
-              Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-              text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+          {description || "No Description"}
           </p>
-          <Author></Author>
+          {author?<Author></Author>:<></>}
         </div>
       </div>
     )
